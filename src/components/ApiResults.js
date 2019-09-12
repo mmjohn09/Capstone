26be46de72f3b30c397bb89180b56d3337901d16
Maggie Johnson <mmjohn09@icloud.com>
2019-09-12 10:03:03 -0500

    added fetch call for user search

--- src/components/ApiResults.js
+++ src/components/ApiResults.js
@@ -4,6 +4,7 @@ export default class ApiResults extends Component {
 
     state = {
         comics: [],
+        volumes: [],
         isLoaded: false,
     }
 
@@ -16,6 +17,18 @@ export default class ApiResults extends Component {
                 this.setState({
                     isLoaded: true,
                     comics: results.results
+                })
+            })
+    }
+
+    getVolumeIssues(userSearch) {
+
+        fetch(`https://cors-anywhere.herokuapp.com/http://comicvine.gamespot.com/api/volumes?limit=100&api_key=29f523a340e2f41efd60b3cbcfb936da5fbba4d2&filter=name:${userSearch}&format=json`)
+            .then(result => result.json())
+            .then(volumes => {
+                this.setState({
+                    isLoaded: true,
+                    volumes: volumes.results
                 })
             })
     }
@@ -34,7 +47,7 @@ export default class ApiResults extends Component {
                     <div className='card-deck'>
                         {this.state.comics.map(comic => (
                             <div className='card' key={comic.id}>
-                                <img className='card-img-top' src={comic.image.thumb_url} />
+                                <img className='card-img-top' src={comic.image.thumb_url} onClick={() => this.getVolumeIssues(comic.name)} />
                                 <div className='card-body'>
                                     <h5 className='card-title'>{comic.name}</h5>
                                 </div>
